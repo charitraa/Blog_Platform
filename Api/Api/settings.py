937070ad45111791cc5ee90439852459dbdcae7f
kenKirito0 <jesishkhadka@gmail.com>
2024-09-18 23:01:35 +0545

    ing new branches

--- Api/Api/settings.py
+++ Api/Api/settings.py
@@ -90,7 +90,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'blog',
         'USER': 'root',
-        'PASSWORD': 'root',
+        'PASSWORD': '',
         'HOST': 'localhost',
         'PORT': '3306',
     }
