b94b22f33a8a959f34cda267c1981b2a658a8b79
charitraa <stharabi9862187405@gmail.com>
2024-09-26 14:22:30 +0545

    extend the time of acces token

--- Api/Api/settings.py
+++ Api/Api/settings.py
@@ -90,7 +90,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'blog',
         'USER': 'root',
-        'PASSWORD': '',
+        'PASSWORD': 'root',
         'HOST': 'localhost',
         'PORT': '3306',
     'OPTIONS': {
